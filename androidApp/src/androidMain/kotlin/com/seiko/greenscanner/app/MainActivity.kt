a11d56dc762462142ce63366cf057e5a4b6c80b4
seiko <seiko_des@outlook.com>
2023-06-07 09:48:36 +0800

    update applicationId

--- androidApp/src/androidMain/kotlin/com/seiko/greenscanner/app/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/seiko/greenscanner/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.myapplication
+package com.seiko.greenscanner.app
 
 import MainView
 import android.os.Bundle
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -63,7 +63,7 @@ kotlin {
 
 android {
     compileSdk = (findProperty("android.compileSdk") as String).toInt()
-    namespace = "com.myapplication.common"
+    namespace = "com.seiko.greenscanner"
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
