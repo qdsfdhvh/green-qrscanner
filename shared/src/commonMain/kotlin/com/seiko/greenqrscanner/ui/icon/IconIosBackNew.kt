4744243d24ed33552508fc7a7663202a835a9b9b
seiko <seiko_des@outlook.com>
2023-06-13 15:18:15 +0800

    update back icon

---
+++ shared/src/commonMain/kotlin/com/seiko/greenqrscanner/ui/icon/IconIosBackNew.kt
@@ -0,0 +1,57 @@
+package com.seiko.greenqrscanner.ui.icon
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun rememberArrowBackIosNew(): ImageVector {
+    return remember {
+        ImageVector.Builder(
+            name = "arrow_back_ios_new",
+            defaultWidth = 24.0.dp,
+            defaultHeight = 24.0.dp,
+            viewportWidth = 24.0f,
+            viewportHeight = 24.0f
+        ).apply {
+            path(
+                fill = SolidColor(Color.Black),
+                fillAlpha = 1f,
+                stroke = null,
+                strokeAlpha = 1f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(15.125f, 21.1f)
+                lineTo(6.7f, 12.7f)
+                quadToRelative(-0.15f, -0.15f, -0.212f, -0.325f)
+                quadToRelative(-0.063f, -0.175f, -0.063f, -0.375f)
+                reflectiveQuadToRelative(0.063f, -0.375f)
+                quadToRelative(0.062f, -0.175f, 0.212f, -0.325f)
+                lineToRelative(8.425f, -8.425f)
+                quadToRelative(0.35f, -0.35f, 0.875f, -0.35f)
+                reflectiveQuadToRelative(0.9f, 0.375f)
+                quadToRelative(0.375f, 0.375f, 0.375f, 0.875f)
+                reflectiveQuadToRelative(-0.375f, 0.875f)
+                lineTo(9.55f, 12f)
+                lineToRelative(7.35f, 7.35f)
+                quadToRelative(0.35f, 0.35f, 0.35f, 0.862f)
+                quadToRelative(0f, 0.513f, -0.375f, 0.888f)
+                reflectiveQuadToRelative(-0.875f, 0.375f)
+                quadToRelative(-0.5f, 0f, -0.875f, -0.375f)
+                close()
+            }
+        }.build()
+    }
+}
--- shared/src/commonMain/kotlin/com/seiko/greenqrscanner/ui/widget/BackButton.kt
+++ shared/src/commonMain/kotlin/com/seiko/greenqrscanner/ui/widget/BackButton.kt
@@ -3,12 +3,11 @@ package com.seiko.greenqrscanner.ui.widget
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import compose.icons.TablerIcons
-import compose.icons.tablericons.ChevronLeft
+import com.seiko.greenqrscanner.ui.icon.rememberArrowBackIosNew
 
 @Composable
 fun BackButton(onClick: () -> Unit) {
     IconButton(onClick) {
-        Icon(TablerIcons.ChevronLeft, "back")
+        Icon(rememberArrowBackIosNew(), "back")
     }
 }
