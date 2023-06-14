fe97b5f83c8290b09319b66e0b200d0d56230ca9
seiko <seiko_des@outlook.com>
2023-06-14 10:25:00 +0800

    fix ios build

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -20,7 +20,7 @@ kotlin {
         version = "1.0.0"
         summary = "Some description for the Shared Module"
         homepage = "Link to the Shared Module homepage"
-        ios.deploymentTarget = "14.1"
+        ios.deploymentTarget = "15.2"
         podfile = project.file("../iosApp/Podfile")
         framework {
             baseName = "shared"
