796f7f87bcae111126f7cb29cf2e3c2487eb3e0d
seiko <seiko_des@outlook.com>
2023-07-27 14:46:46 +0800

    update settings scene ui

--- shared/src/commonMain/kotlin/com/seiko/greenqrscanner/ui/scene/settings/SettingsScene.kt
+++ shared/src/commonMain/kotlin/com/seiko/greenqrscanner/ui/scene/settings/SettingsScene.kt
@@ -8,44 +8,44 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.moriatsushi.koject.compose.rememberInject
+import com.seiko.greenqrscanner.data.store.DataStoreKeys
+import com.seiko.greenqrscanner.data.store.DataStoreManager
 import com.seiko.greenqrscanner.ui.widget.preference.SwitchPreference
 
 @Composable
 fun SettingsScene() {
     Scaffold { innerPadding ->
+        val dataStoreManager: DataStoreManager = rememberInject()
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(6.dp),
             contentPadding = PaddingValues(8.dp),
             modifier = Modifier.padding(innerPadding).fillMaxSize(),
         ) {
             item {
-                var saveBarcodeToHistory by rememberSaveable { mutableStateOf(true) }
                 SwitchPreference(
                     title = { Text("将扫描添加到历史") },
-                    checked = saveBarcodeToHistory,
-                    onCheckedChange = { saveBarcodeToHistory = it },
+                    dataStore = dataStoreManager,
+                    key = DataStoreKeys.save_barcode_to_history,
+                    defaultValue = true,
                 )
             }
             item {
-                var vibrateOnScan by rememberSaveable { mutableStateOf(false) }
                 SwitchPreference(
                     title = { Text("振动") },
-                    checked = vibrateOnScan,
-                    onCheckedChange = { vibrateOnScan = it },
+                    dataStore = dataStoreManager,
+                    key = DataStoreKeys.vibrate_on_scan,
+                    defaultValue = false,
                 )
             }
             item {
-                var soundOnScan by rememberSaveable { mutableStateOf(false) }
                 SwitchPreference(
                     title = { Text("提示音") },
-                    checked = soundOnScan,
-                    onCheckedChange = { soundOnScan = it },
+                    dataStore = dataStoreManager,
+                    key = DataStoreKeys.sound_on_scan,
+                    defaultValue = false,
                 )
             }
         }
--- shared/src/commonMain/kotlin/com/seiko/greenqrscanner/ui/widget/preference/SwitchPreference.kt
+++ shared/src/commonMain/kotlin/com/seiko/greenqrscanner/ui/widget/preference/SwitchPreference.kt
@@ -4,7 +4,30 @@ import androidx.compose.foundation.clickable
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.seiko.greenqrscanner.data.store.DataStoreManager
+
+@Composable
+fun SwitchPreference(
+    title: @Composable () -> Unit,
+    key: String,
+    defaultValue: Boolean,
+    dataStore: DataStoreManager,
+    modifier: Modifier = Modifier,
+    description: (@Composable () -> Unit)? = null,
+) {
+    SwitchPreference(
+        title = title,
+        checked = remember(key) {
+            dataStore.flow(key, defaultValue)
+        }.collectAsState(false).value,
+        onCheckedChange = { dataStore.putAsync(key, it) },
+        modifier = modifier,
+        description = description,
+    )
+}
 
 @Composable
 fun SwitchPreference(
