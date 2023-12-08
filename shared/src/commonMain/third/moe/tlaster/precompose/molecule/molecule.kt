de2dd6db81033c0f3814884ac070db54e3a20bd7
seiko <seiko_des@outlook.com>
2023-12-08 21:16:14 +0800

    move PresenterHolder stateFlow to state

--- shared/src/commonMain/third/moe/tlaster/precompose/molecule/molecule.kt
+++ shared/src/commonMain/third/moe/tlaster/precompose/molecule/molecule.kt
@@ -2,9 +2,10 @@ package moe.tlaster.precompose.molecule
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MonotonicFrameClock
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.currentCompositeKeyHash
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import app.cash.molecule.RecompositionMode
 import app.cash.molecule.launchMolecule
@@ -16,18 +17,37 @@ import kotlin.coroutines.CoroutineContext
 internal expect fun providePlatformDispatcher(): CoroutineContext
 
 @OptIn(ExperimentalStdlibApi::class)
-private class PresenterHolder<T>(
+class PresenterHolder<T>(
     body: @Composable () -> T,
     useImmediateClock: Boolean = false,
 ) : AutoCloseable {
-    private val dispatcher = providePlatformDispatcher()
-    private val clock = if (useImmediateClock || dispatcher[MonotonicFrameClock] == null) {
-        RecompositionMode.Immediate
-    } else {
-        RecompositionMode.ContextClock
+
+    private val scope: CoroutineScope
+
+    private var _state: MutableState<T>? = null
+    val state: State<T> get() = _state!!
+
+    init {
+        val dispatcher = providePlatformDispatcher()
+        scope = CoroutineScope(dispatcher)
+        scope.launchMolecule(
+            mode = if (useImmediateClock || dispatcher[MonotonicFrameClock] == null) {
+                RecompositionMode.Immediate
+            } else {
+                RecompositionMode.ContextClock
+            },
+            body = body,
+            emitter = { value ->
+                val outputState = _state
+                if (outputState != null) {
+                    outputState.value = value
+                } else {
+                    _state = mutableStateOf(value)
+                }
+            },
+        )
     }
-    private val scope = CoroutineScope(dispatcher)
-    val state = scope.launchMolecule(mode = clock, body = body)
+
     override fun close() {
         scope.cancel()
     }
@@ -42,8 +62,7 @@ fun <T> producePresenter(
     val key = remember {
         PresenterHolder::class.simpleName + "@" + keyHash.toString(36)
     }
-    val stateHolder = LocalStateHolder.current
-    return stateHolder.getOrPut(key) {
+    return LocalStateHolder.current.getOrPut(key) {
         PresenterHolder(body, useImmediateClock)
-    }.state.collectAsState()
+    }.state
 }
