d00f4663f43164556f2ed0fd41959c1744e985c6
seiko <seiko_des@outlook.com>
2023-12-12 19:42:50 +0800

    remove build-licenses-json.sh

--- build-licenses-json.sh
+++
@@ -1,4 +0,0 @@
-./gradlew :shared:exportLibraryDefinitions -PaboutLibraries.exportPath=src/commonMain/resources/ -PaboutLibraries.exportVariant=release
-
-# TODO: ios only support resources in pod library
-cp shared/src/commonMain/resources/aboutlibraries.json iosAppCombine/src/commonMain/resources/aboutlibraries.json
